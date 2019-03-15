2a04d5db4d6a16d117b307d908f81c2cd4033909
Stephen Shank <sshank314@gmail.com>
2019-03-15 13:29:36 -0400

    Extracting relevant parts of haplotype-reconstruction repository.

---
+++ acme.sh
@@ -0,0 +1,4 @@
+make
+python write_config.py
+./aBayesQR acme_config
+python parse_output.py
---
+++ parse_output.py
@@ -0,0 +1,17 @@
+def parse_abayesqr_output(input_text, output_fasta):
+    with open(input_text) as input_file:
+        lines = input_file.readlines()
+    records = []
+    for i, line in enumerate(lines):
+        if i % 2 == 0:
+            freq = float(line.split()[-1])
+            number = int(i/2)+1
+            header = 'haplotype-%d_freq-%f' % (number, freq)
+        if i % 2 == 1:
+            seq = Seq(line.strip())
+            record = SeqRecord(seq, id=header, description='')
+            records.append(record)
+    SeqIO.write(records, output_fasta, 'fasta')
+
+if __name__ == '__main__':
+    parse_abayesqr_output("test_ViralSeq.txt", "haplotypes.fasta")
